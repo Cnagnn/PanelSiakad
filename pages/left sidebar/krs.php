f419777d7d5aeefa859c78fe9980c0fefd5e49d9
Cnagnn <yogapratamaputrar@gmail.com>
2024-04-04 20:46:19 +0700

    update

--- pages/left sidebar/krs.php
+++ pages/left sidebar/krs.php
@@ -161,7 +161,7 @@ $result = mysqli_query($con, "SELECT * FROM krs");
           <div class="col-12">
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Kartu Rencana Studi Semester Genap 2023-2024</h3>
+                <h3 class="card-title"><b>Kartu Rencana Studi Semester Genap 2023-2024</b></h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
